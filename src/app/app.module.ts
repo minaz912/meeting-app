96bf1d3bc69176252786d4ebdf94c4885159b9b4
Mina S. Aziz <minaz922@gmail.com>
2017-04-15 22:37:27 +0200

    import MaterialModule and FlexLayoutModule in the root Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { MaterialModule } from './shared/material/material.module';
 import { SDKBrowserModule } from './shared/sdk';
 
 import { AppComponent } from './app.component';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -13,8 +18,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
+    MaterialModule,
+    FlexLayoutModule,
     SDKBrowserModule.forRoot()
   ],
   providers: [],
