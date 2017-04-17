92719e3248e6657a168d9609dd1e9cbc1ff5e900
Mina S. Aziz <minaz922@gmail.com>
2017-04-17 21:25:42 +0200

    create a home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule, RouterLink, RouterLinkActive } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
 import { CreateMeetingComponent } from './create-meeting/create-meeting.component';
 
 const routes: Routes = [
   {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
     path: 'create-meeting',
     component: CreateMeetingComponent
+  },
+  {
+    path: '*',
+    redirectTo: '/',
+    pathMatch: 'full'
   }
 ];
 
